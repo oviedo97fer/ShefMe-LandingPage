00b3d7029f314deb23941c6f7a5974205c6917b1
oviedo97fer <fernando.n.oviedo@gmail.com>
2019-09-19 16:30:02 -0300

    Cards finished

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,23 +2,23 @@ import React from 'react';
 import dotsDown from '../../assets/dotsDown.png'
 
 import './style.css'
-const Card = ({src, dots=false, title, description}) =>{
+
+const Card = ({src, dots, title, description}) =>{
 
     return(
-        <div style={{display: 'flex', position: 'absolute',}}>
-        <div className='cardComp'>
-            <img className='iconCard' src={src}/>
-            <div className='cardDesc'>
-                <h2>{title}</h2>
-                <p>{description}</p>
+        <div className='Card'>
+            <div className='container'>
+                <div>
+                    <img className='iconCard' src={src}/>
+                    <div className='cardText'>
+                        <h1>{title}</h1>
+                        <p>{description}</p>
+                    </div>
+                </div>
             </div>
-            
-        </div>
-        {dots? 
-                <img className='dotsCard' src={dotsDown} alt='dots'/>
-            :
-            null
-            }
+            {dots? 
+            <img className='dotsCard' src={dotsDown} alt='dots'/>
+            : null}
         </div>
     )
 }
